b356e64037756b99083dcaf595b108be4f4e1183
Isaac Jiang <isaacjiang@gmail.com>
2019-11-13 16:18:55 -0800

    Updates

---
+++ precache-manifest.d412fd216b044aa46e9667bb50e5005f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2585d5f54e19083d0aac6b189afadffc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b8367e88cd17a1b7f3f4",
+    "url": "/my-app/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "077186ac8f96d354b557",
+    "url": "/my-app/static/css/main.09f1d7aa.chunk.css"
+  },
+  {
+    "revision": "b8367e88cd17a1b7f3f4",
+    "url": "/my-app/static/js/2.9a650b4a.chunk.js"
+  },
+  {
+    "revision": "077186ac8f96d354b557",
+    "url": "/my-app/static/js/main.a167a3d6.chunk.js"
+  },
+  {
+    "revision": "a65e55756f41965287cf",
+    "url": "/my-app/static/js/runtime-main.625c24d2.js"
+  }
+]);
